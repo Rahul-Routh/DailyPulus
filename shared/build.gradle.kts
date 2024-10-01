86006efd373234627442b8bcd87fcc7d46aec5a7
itsra <its.rahulrouth@gmail.com>
2024-10-01 08:34:21 +0530

    jwifji

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -26,6 +26,7 @@ kotlin {
     sourceSets {
         commonMain.dependencies {
             //put your multiplatform dependencies here
+            implementation(libs.koin.core)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
